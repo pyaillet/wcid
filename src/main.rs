9c461530d36bbc9a8797de554f10d1ff4da195ab
Pierre-Yves Aillet <pyaillet@gmail.com>
2021-05-07 22:40:28 +0200

    :recycle: use SelfSubjectAccessReview

--- src/main.rs
+++ src/main.rs
@@ -1,146 +1,126 @@
-use anyhow::Result;
-use k8s_openapi::api::{apps::v1::Deployment, core::v1::Pod};
-use kube::{
-    api::{Api, ListParams, ObjectList},
-    Client, Resource,
-};
-use serde;
+use kube::Client;
+use serde_json::json;
 
-#[derive(Clone, Copy, Debug)]
-enum Verb {
-    Get,
-    List,
+use k8s_openapi::api::authorization::v1::SelfSubjectAccessReview;
+
+#[derive(Clone)]
+struct ResourceAttributes {
+    group: String,
+    resource: String,
+    namespace: Option<String>,
+    verb: String,
 }
 
-#[derive(Clone, Debug)]
-struct Request {
-    kind: String,
-    verb: Verb,
+#[derive(Clone)]
+struct ResourceAttributesBuilder {
+    group: Option<String>,
+    resource: Option<String>,
+    namespace: Option<String>,
+    verb: Option<String>,
 }
 
-#[derive(Clone, Copy, Debug)]
-enum MyResult {
-    Ok,
-    Forbidden,
-    Error,
-}
-
-enum ObjectResult<T: Clone> {
-    Unit(Result<T, kube::Error>),
-    List(Result<ObjectList<T>, kube::Error>),
-}
-
-#[derive(Clone, Debug)]
-struct RequestResult {
-    request: Request,
-    result: MyResult,
-}
-
-impl RequestResult {
-    fn new(request: Request, result: MyResult) -> Self {
-        Self { request, result }
-    }
-
-    fn forbidden(request: Request) -> Self {
-        RequestResult::new(request, MyResult::Forbidden)
-    }
-
-    fn ok(request: Request) -> Self {
-        RequestResult::new(request, MyResult::Ok)
-    }
-
-    fn error(request: Request) -> Self {
-        RequestResult::new(request, MyResult::Error)
-    }
-}
-
-struct TesterNamespace<T> {
-    object: Option<T>,
-    namespace: String,
-}
-
-impl<T: Resource> TesterNamespace<T>
-where
-    <T as Resource>::DynamicType: Default,
-    T: Clone,
-    T: std::fmt::Debug,
-    T: serde::de::DeserializeOwned,
-{
-    fn new(ns: &str) -> Self {
+impl ResourceAttributesBuilder {
+    fn new() -> Self {
         Self {
-            object: None,
-            namespace: ns.to_string(),
+            group: None,
+            resource: None,
+            namespace: None,
+            verb: None,
         }
     }
 
-    fn name(&self) -> String {
-        match &self.object {
-            Some(o) => o.name(),
-            _ => "NOT FOUND".to_string(),
-        }
+    fn namespace(&mut self, namespace: String) -> Self {
+        self.namespace = Some(namespace);
+        self.clone()
     }
 
-    fn handle_result(req: Request, result: ObjectResult<T>) -> (RequestResult, Option<T>) {
-        match result {
-            ObjectResult::Unit(Ok(_)) => (RequestResult::ok(req), None),
-            ObjectResult::List(Ok(l)) => (RequestResult::ok(req), l.items.first().cloned()),
-            ObjectResult::Unit(Err(kube::Error::Api(ae)))
-            | ObjectResult::List(Err(kube::Error::Api(ae))) => {
-                if ae.code == 403 {
-                    (RequestResult::forbidden(req), None)
-                } else {
-                    (RequestResult::error(req), None)
-                }
-            }
-            _ => (RequestResult::error(req), None),
-        }
+    fn group(&mut self, group: String) -> Self {
+        self.group = Some(group);
+        self.clone()
     }
 
-    async fn test_all(&mut self) -> Vec<RequestResult> {
-        let mut v = Vec::new();
-        v.push(self.test_list().await);
-        v.push(self.test_get().await);
-        v
+    fn verb(&mut self, verb: String) -> Self {
+        self.verb = Some(verb);
+        self.clone()
     }
 
-    async fn test_get(&self) -> RequestResult {
-        let req = Request {
-            kind: std::any::type_name::<T>().to_string(),
-            verb: Verb::Get,
-        };
-        if let Ok(client) = Client::try_default().await {
-            let c_api: Api<T> = Api::namespaced(client, &self.namespace);
-            Self::handle_result(req, ObjectResult::Unit(c_api.get(&self.name()).await)).0
-        } else {
-            RequestResult::error(req)
-        }
+    fn resource(&mut self, resource: String) -> Self {
+        self.resource = Some(resource);
+        self.clone()
     }
 
-    async fn test_list(&mut self) -> RequestResult {
-        let req = Request {
-            kind: std::any::type_name::<T>().to_string(),
-            verb: Verb::List,
-        };
-        if let Ok(client) = Client::try_default().await {
-            let c_api: Api<T> = Api::namespaced(client, &self.namespace);
-            let (result, object) = Self::handle_result(
-                req,
-                ObjectResult::List(c_api.list(&ListParams::default()).await),
-            );
-            self.object = object;
-            result
-        } else {
-            RequestResult::error(req)
+    fn build(self) -> ResourceAttributes {
+        let group = self
+            .group
+            .expect("Incomplete resource attribute declaration");
+        let resource = self
+            .resource
+            .expect("Incomplete resource attribute declaration");
+        let verb = self
+            .verb
+            .expect("Incomplete resource attribute declaration");
+        ResourceAttributes {
+            group,
+            resource,
+            verb,
+            namespace: self.namespace,
         }
+    }
+}
+
+async fn check(review: &ResourceAttributes) -> bool {
+    let client = match Client::try_default().await {
+        Ok(client) => client,
+        Err(_e) => {
+            panic!("Cannot create client");
+        }
+    };
+
+    let ssar: SelfSubjectAccessReview = match serde_json::from_value(json!({
+        "apiVersion": "authorization.k8s.io/v1",
+        "kind": "SelfSubjectAccessReview",
+        "metadata": {},
+        "spec": {
+            "resourceAttributes": {
+              "group": review.group,
+              "resource": review.resource,
+              "namespace": review.namespace,
+              "verb": review.verb,
+            },
+        }
+    })) {
+        Ok(ssar) => ssar,
+        Err(e) => {
+            println!("Error {:?}", e);
+            return false;
+        }
+    };
+
+    let result =
+        SelfSubjectAccessReview::create_self_subject_access_review(&ssar, Default::default());
+    match result {
+        Ok((reqp, _)) => {
+            let res = client
+                .request::<SelfSubjectAccessReview>(
+                    http::Request::post(reqp.uri())
+                        .body(reqp.body().clone())
+                        .unwrap(),
+                )
+                .await
+                .unwrap();
+            res.status.unwrap().allowed
+        }
+        Err(_) => return false,
     }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut tp = TesterNamespace::<Pod>::new("default");
-    println!("{:?}", tp.test_all().await);
-    let mut td = TesterNamespace::<Deployment>::new("default");
-    println!("{:?}", td.test_all().await);
-
+    let ra = ResourceAttributesBuilder::new()
+        .group("".into())
+        .resource("pods".into())
+        .verb("get".into())
+        .build();
+    println!("{:?}", check(&ra).await);
     Ok(())
 }
