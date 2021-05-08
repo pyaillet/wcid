22d2d4d254522782f38506d169d43386273c1f1b
Pierre-Yves Aillet <pyaillet@gmail.com>
2021-05-08 23:57:51 +0200

    :sparkles: working with parameters and ns

--- src/check.rs
+++ src/check.rs
@@ -1,11 +1,11 @@
 use std::{collections::HashMap, fmt::Display};
 
 use anyhow::Result;
+use comfy_table::{presets::NOTHING, Attribute, Cell, Color, Table};
 use futures::future::try_join_all;
 use kube::Client;
 use kube::{api::DynamicObject, Resource};
 use kube::{api::GroupVersionKind, client::Discovery};
-use prettytable::{format, Cell, Row, Table};
 use serde_json::json;
 
 use k8s_openapi::api::authorization::v1::SelfSubjectAccessReview;
@@ -15,32 +15,22 @@ const ALL_VERBS: [&str; 7] = [
 ];
 
 #[derive(Clone, Debug)]
-pub struct Config {
-    pub display_group: bool,
-    pub namespace: Option<String>,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            display_group: false,
-            namespace: None,
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
 pub struct ResourceCheckResult {
     pub gvk: GroupVersionKind,
     pub results: HashMap<&'static str, CheckResult>,
 }
 
 pub trait GroupVersionKindHelper {
+    fn plural(&self) -> String;
     fn kind(&self) -> String;
     fn group(&self) -> String;
 }
 
 impl GroupVersionKindHelper for GroupVersionKind {
+    fn plural(&self) -> String {
+        DynamicObject::plural(&self).to_string()
+    }
+
     fn kind(&self) -> String {
         DynamicObject::kind(&self).to_string()
     }
@@ -51,6 +41,10 @@ impl GroupVersionKindHelper for GroupVersionKind {
 }
 
 impl GroupVersionKindHelper for ResourceCheckResult {
+    fn plural(&self) -> String {
+        DynamicObject::plural(&self.gvk).to_string()
+    }
+
     fn kind(&self) -> String {
         self.gvk.kind()
     }
@@ -87,9 +81,9 @@ async fn check_resource_verb(
         "spec": {
             "resourceAttributes": {
               "group": gvk.group(),
-              "resource": gvk.kind(),
+              "resource": gvk.plural(),
               "namespace": namespace,
-              "verb": verb,
+              "verb": verb.to_ascii_lowercase(),
             },
         }
     }))?;
@@ -128,10 +122,6 @@ async fn check_resource(
     })
 }
 
-async fn check_resource_global(gvk: &GroupVersionKind) -> Result<ResourceCheckResult> {
-    check_resource(gvk, None).await
-}
-
 async fn list_resources() -> Result<Vec<GroupVersionKind>> {
     let client = Client::try_default().await?;
 
@@ -147,39 +137,58 @@ async fn list_resources() -> Result<Vec<GroupVersionKind>> {
     Ok(v)
 }
 
-pub async fn check_all() -> Result<FullResult> {
-    let resources = list_resources().await?;
-    let future_results: Vec<_> = resources
-        .iter()
-        .map(|gvk| check_resource_global(gvk))
-        .collect();
-    let results = try_join_all(future_results).await?;
+#[derive(Clone, Debug)]
+pub struct Config {
+    pub display_group: bool,
+    pub namespace: Option<String>,
+}
 
-    Ok(FullResult {
-        config: Config::default(),
-        results,
-    })
+pub struct Checker {
+    pub config: Config,
+}
+
+impl Checker {
+    pub fn new(config: Config) -> Self {
+        Self { config }
+    }
+
+    pub async fn check_all(&self) -> Result<FullResult> {
+        let resources = list_resources().await?;
+        let future_results: Vec<_> = resources
+            .iter()
+            .map(|gvk| check_resource(gvk, self.config.namespace.clone()))
+            .collect();
+        let results = try_join_all(future_results).await?;
+
+        Ok(FullResult {
+            config: self.config.clone(),
+            results,
+        })
+    }
 }
 
 impl Display for FullResult {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut rows: Vec<Row> = Vec::new();
+        let mut table = Table::new();
+        table.load_preset(NOTHING);
 
-        let mut titles = Vec::new();
+        let column_count = if self.config.display_group { 9 } else { 8 };
+
+        let mut titles = Vec::with_capacity(column_count);
         if self.config.display_group {
-            titles.push(Cell::new("Group").style_spec("b"));
+            titles.push(Cell::new("Group").add_attribute(Attribute::Bold));
         }
-        titles.push(Cell::new("Kind").style_spec("b"));
+        titles.push(Cell::new("Kind").add_attribute(Attribute::Bold));
         titles.extend(
             ALL_VERBS
                 .iter()
-                .map(|v| Cell::new(v).style_spec("b"))
+                .map(|v| Cell::new(v).add_attribute(Attribute::Bold))
                 .collect::<Vec<Cell>>(),
         );
-        rows.push(Row::new(titles));
+        table.set_header(titles);
 
-        rows.extend(self.results.iter().map(|result| {
-            let mut row = Vec::new();
+        self.results.iter().for_each(|result| {
+            let mut row: Vec<Cell> = Vec::with_capacity(column_count);
             if self.config.display_group {
                 row.push(Cell::new(&result.group()));
             }
@@ -190,22 +199,21 @@ impl Display for FullResult {
                     .map(|v| match &result.results.get(v) {
                         Some(r) => {
                             if r.allowed {
-                                Cell::new("✔").style_spec("Fgc")
+                                Cell::new("✔").fg(Color::Green)
                             } else {
-                                Cell::new("✖").style_spec("Frc")
+                                Cell::new("✖").fg(Color::Red)
                             }
                         }
-                        None => Cell::new("✖").style_spec("Frc"),
+                        None => {
+                            println!("Not found");
+                            Cell::new("✖").fg(Color::Red)
+                        }
                     })
                     .collect::<Vec<Cell>>(),
             );
-            Row::new(row)
-        }));
+            table.add_row(row);
+        });
 
-        let mut table = Table::init(rows);
-
-        let format = format::FormatBuilder::new().padding(1, 1).build();
-        table.set_format(format);
         table.fmt(f)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use anyhow::Result;
+use clap::{AppSettings, Clap};
 mod check;
+
+#[derive(Clap)]
+#[clap(
+    version = "1.0",
+    author = "Pierre-Yves Aillet <pyaillet@gmail.com>",
+    about = "\n\nWCID What Can I Do is an RBAC enumerator for Kubernetes"
+)]
+#[clap(setting = AppSettings::ColoredHelp)]
+pub struct Opts {
+    #[clap(short, long)]
+    pub display_group: bool,
+    #[clap(short, long)]
+    pub namespace: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let result = check::check_all().await?;
+    let opts: Opts = Opts::parse();
+    let config = check::Config {
+        display_group: opts.display_group,
+        namespace: opts.namespace,
+    };
+    let checker = check::Checker::new(config);
+    let result = checker.check_all().await?;
     println!("{}", result);
     Ok(())
 }
