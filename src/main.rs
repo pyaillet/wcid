451bed0a26e306d73b88815d957aac8febc2a696
Pierre-Yves Aillet <pyaillet@users.noreply.github.com>
2021-05-09 10:33:25 +0200

    :sparkles: short output (#6)

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,8 @@ pub struct Opts {
     pub namespace: Option<String>,
     #[clap(short, long, default_value = "pretty")]
     pub format: String,
+    #[clap(short = 's', long)]
+    pub hide_forbidden: bool,
 }
 
 #[tokio::main]
@@ -24,6 +26,7 @@ async fn main() -> Result<()> {
     let config = check::config::Config {
         display_group: opts.display_group,
         namespace: opts.namespace,
+        hide_forbidden: opts.hide_forbidden,
     };
     let checker = check::Checker::new(config.clone());
     let result = checker.check_all().await?;
