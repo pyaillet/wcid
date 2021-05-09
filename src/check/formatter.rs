16ce2fb1189d503d763d30f47969741797ff6c08
Pierre-Yves Aillet <pyaillet@gmail.com>
2021-05-09 09:36:55 +0200

    :sparkles: add json output

--- src/check/formatter.rs
+++ src/check/formatter.rs
@@ -1,3 +1,32 @@
+use std::fmt::Display;
+
+use super::config;
+use super::types;
+
+pub enum Formatter {
+    Pretty(pretty::Pretty),
+    Json(json::Json),
+}
+
+impl Formatter {
+    pub fn new(format: String, config: config::Config, result: types::FullResult) -> Self {
+        if format == "json" {
+            Formatter::Json(json::Json::new(result))
+        } else {
+            Formatter::Pretty(pretty::Pretty::new(config, result))
+        }
+    }
+}
+
+impl Display for Formatter {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Formatter::Pretty(p) => p.fmt(f),
+            Formatter::Json(j) => j.fmt(f),
+        }
+    }
+}
+
 pub mod pretty {
 
     use super::super::config;
@@ -72,8 +101,74 @@ pub mod pretty {
     }
 }
 
-//mod json {
-//    struct Json {}
-//
-//    impl Display for Pretty {}
-//}
+pub mod json {
+
+    use std::collections::HashMap;
+    use std::fmt::Display;
+
+    use serde::Serialize;
+
+    use super::super::types;
+
+    use crate::check::types::GroupVersionKindHelper;
+
+    pub struct Json {
+        result: JsonFullResult,
+    }
+
+    #[derive(Serialize)]
+    struct JsonResourceResult {
+        group: String,
+        kind: String,
+        verb_allowed: HashMap<&'static str, bool>,
+    }
+
+    #[derive(Serialize)]
+    struct JsonFullResult {
+        items: Vec<JsonResourceResult>,
+    }
+
+    impl From<types::ResourceCheckResult> for JsonResourceResult {
+        fn from(value: types::ResourceCheckResult) -> Self {
+            let verb_allowed = value
+                .items
+                .into_iter()
+                .map(|(k, v)| (k, v.allowed))
+                .collect::<HashMap<&'static str, bool>>();
+            Self {
+                group: value.gvk.group(),
+                kind: value.gvk.kind(),
+                verb_allowed,
+            }
+        }
+    }
+
+    impl From<types::FullResult> for JsonFullResult {
+        fn from(value: types::FullResult) -> Self {
+            Self {
+                items: value
+                    .items
+                    .into_iter()
+                    .map(|i| i.into())
+                    .collect::<Vec<JsonResourceResult>>(),
+            }
+        }
+    }
+
+    impl Json {
+        pub fn new(full_result: types::FullResult) -> Self {
+            Self {
+                result: full_result.into(),
+            }
+        }
+    }
+
+    impl Display for Json {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match serde_json::to_string(&self.result) {
+                Ok(output) => f.write_str(&output),
+                Err(_e) => Err(std::fmt::Error),
+            }
+        }
+    }
+}
